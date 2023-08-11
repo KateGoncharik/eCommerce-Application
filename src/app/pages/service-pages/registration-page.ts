87f519720b90dd247ccb581cf87b6ba3c04e017b
laleks6 <lomanovsaha98@gmail.com>
2023-08-12 00:06:44 +0300

    feat: create input

--- src/app/pages/service-pages/registration-page.ts
+++ src/app/pages/service-pages/registration-page.ts
@@ -1,31 +1,45 @@
 import { Page } from '@templates/page';
-import { el, mount } from "redom";
+import { el, mount } from 'redom';
 
 class RegistrationPage extends Page {
   protected textObject = {
     title: 'Registration page',
   };
-  
+
   private createForm(): HTMLElement {
-    const blockForm = el(".block-form");
-    const form = el(".form");
-   
+    const blockForm = el('.block-form');
+    const form = el('.form');
+    const blockInput = this.createInput();
 
     mount(blockForm, form);
-
+    mount(form, blockInput);
     return blockForm;
   }
 
+  private createInput(): HTMLElement {
+    const blockInput = el('.blockInput');
+    const blockName = el('.form__input-name');
+    const blockPassword = el('.form__input-password');
+    const emailInput = el('input', { class: 'input__registration-name', type: 'text', placeholder: 'name' });
+    const passwordInput = el('input', {
+      class: 'input__registration-password',
+      type: 'password',
+      placeholder: 'password',
+    });
 
-
+    mount(blockInput, blockName);
+    mount(blockInput, blockPassword);
+    mount(blockName, emailInput);
+    mount(blockPassword, passwordInput);
+    return blockInput;
+  }
   protected build(): HTMLElement {
-    const wrapper = el(".form-wrapper");
+    const wrapper = el('.form-wrapper');
     const form = this.createForm();
 
     mount(wrapper, form);
     return wrapper;
   }
-  
 }
 
 export { RegistrationPage };
