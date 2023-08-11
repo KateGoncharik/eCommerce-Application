077e02824777a50a72271bd682e0bb9437caa17a
laleks6 <lomanovsaha98@gmail.com>
2023-08-11 23:42:16 +0300

    feat: create form

--- src/app/pages/service-pages/registration-page.ts
+++ src/app/pages/service-pages/registration-page.ts
@@ -1,9 +1,31 @@
 import { Page } from '@templates/page';
+import { el, mount } from "redom";
 
 class RegistrationPage extends Page {
   protected textObject = {
     title: 'Registration page',
   };
+  
+  private createForm(): HTMLElement {
+    const blockForm = el(".block-form");
+    const form = el(".form");
+   
+
+    mount(blockForm, form);
+
+    return blockForm;
+  }
+
+
+
+  protected build(): HTMLElement {
+    const wrapper = el(".form-wrapper");
+    const form = this.createForm();
+
+    mount(wrapper, form);
+    return wrapper;
+  }
+  
 }
 
 export { RegistrationPage };
