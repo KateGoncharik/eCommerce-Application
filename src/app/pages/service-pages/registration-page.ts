8bf57a7522afd4bd90cb1fb3d921882b0813919e
laleks6 <lomanovsaha98@gmail.com>
2023-08-12 15:53:04 +0300

    refactor: change type and class name for page and registration-page

--- src/app/pages/service-pages/registration-page.ts
+++ src/app/pages/service-pages/registration-page.ts
@@ -7,10 +7,10 @@ class RegistrationPage extends Page {
   };
 
   private createForm(): HTMLElement {
-    const blockForm = el('.block-form');
+    const blockForm = el('.form-block');
     const form = el('.form');
     const blockInput = this.createInput();
-    const title = this.header();
+    const title = this.title();
     const blockButton = this.createButton();
 
     mount(blockForm, form);
@@ -21,38 +21,34 @@ class RegistrationPage extends Page {
   }
 
   private createInput(): HTMLElement {
-    const blockInput = el('.blockInput');
-    const blockName = el('.form__input-name');
-    const blockPassword = el('.form__input-password');
-    const emailInput = el('input', { class: 'input__registration-name', type: 'text', placeholder: 'name' });
-    const passwordInput = el('input', {
-      class: 'input__registration-password',
-      type: 'password',
-      placeholder: 'password',
-    });
+    const blockInput = el('.inputs-block');
+    // const blockName = el('.block-input-name');
+    // const blockPassword = el('.block-input-password');
+    const nameInput = el('input', { class: 'name-input input', type: 'text', placeholder: 'name' });
+    const passwordInput = el('input', { class: 'password-input input', type: 'password', placeholder: 'password' });
 
-    mount(blockInput, blockName);
-    mount(blockInput, blockPassword);
-    mount(blockName, emailInput);
-    mount(blockPassword, passwordInput);
+    mount(blockInput, nameInput);
+    mount(blockInput, passwordInput);
+    // mount(blockName, nameInput);
+    // mount(blockPassword, passwordInput);
     return blockInput;
   }
 
-  protected header(): HTMLElement {
-    const blockTitle = el('h2', { class: 'header__registration' });
+  protected title(): HTMLElement {
+    const blockTitle = el('h2', { class: 'form-title title' });
 
     blockTitle.textContent = 'Sign up';
     return blockTitle;
   }
 
   private createButton(): HTMLElement {
-    const blockInput = el('.blockButton');
-    const button = el('button', { class: 'btn__registration' });
+    // const blockButton = el('.blockButton');
+    const button = el('button', { class: 'form-button btn' });
 
     button.textContent = 'Join us';
 
-    mount(blockInput, button);
-    return blockInput;
+    // mount(blockButton, button);
+    return button;
   }
 
   protected build(): HTMLElement {
