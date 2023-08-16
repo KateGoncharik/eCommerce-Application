e8f17e70acd6090ec8a7b60f594f7e1f4b08f9b0
Aleksandr <96025122+laleks6@users.noreply.github.com>
2023-08-16 16:27:02 +0300

    feat: add check all form validation

--- src/app/pages/service-pages/registration-page.ts
+++ src/app/pages/service-pages/registration-page.ts
@@ -61,12 +61,15 @@ class RegistrationPage extends Page {
   }
 
   protected title(): HTMLElement {
-    const blockTitle = el('h2', 'Sign up',{ class: 'form-title title' });
+    const blockTitle = el('h2', 'Sign up', { class: 'form-title title' });
     return blockTitle;
   }
 
   private createButton(): HTMLElement {
     const button = el('button', 'Join us', { type: 'submit', class: 'form-btn btn' });
+
+    this.validation.checkValidationAllForm(button)
+
     return button;
   }
 
--- src/app/validation-registration-form.ts
+++ src/app/validation-registration-form.ts
@@ -2,7 +2,7 @@ import { Shemas, dataValue } from '../schemas/schemas-registration-form';
 
 export class ValidationForm {
 
-  public getValidation(userData: Shemas, element: Element, showElement: Element): void {
+  public checkValidation(userData: Shemas, element: Element, showElement: Element): void {
     const validationResult = Shemas.safeParse(userData);
     const showBlock = showElement as HTMLElement
 
@@ -15,10 +15,10 @@ export class ValidationForm {
         if(arrayErrors !== undefined){
           showBlock.style.display = 'flex';
           showBlock.textContent = this.showErrors(arrayErrors);
+
+          element.classList.remove('input-valid');
+          element.classList.add('input-error');
         }
-  
-        element.classList.remove('input-valid');
-        element.classList.add('input-error');
       }
     } else {
       showBlock.textContent = '';
@@ -29,34 +29,61 @@ export class ValidationForm {
     }
   }
 
-  public eventInput(element: HTMLElement): void {
+  public eventInput(blockRegistration: HTMLElement): void {
  
-    Array.from(element.children).forEach((el) => {
-      Array.from(el.children).forEach((input) => {
+    Array.from(blockRegistration.children).forEach((blockInputs) => {
+      Array.from(blockInputs.children).forEach((input) => {
 
         if (!(input instanceof HTMLInputElement)) {
           return;
         }
 
         input.addEventListener('input', () => {
-      
-          const placeholder = input.placeholder;
-          const showBlock = document.getElementsByClassName(`show-validation-${input.classList[0]}`)[0]!;
-
-          const data: dataValue = {
-            [placeholder]: input.value
-          };
-          
-          this.getValidation(data, input, showBlock);
+          this.checkChangeInput(input)
         })
       });
     });
   }
 
-  public showErrors(arr: string[]): string {
-    const createError = arr[0]
-    return createError
+  private checkChangeInput(input: HTMLInputElement): void {
+    const placeholder = input.placeholder;
+    const showErrorBlock = document.getElementsByClassName(`show-validation-${input.classList[0]}`)[0]!;
+
+    const data: dataValue = {
+      [placeholder]: input.value
+    };
+          
+    this.checkValidation(data, input, showErrorBlock);
   }
 
-  
+  public checkValidationAllForm(elementBtn: HTMLElement): void {
+    const inputs = document.getElementsByClassName('input');
+    let countTrue:number;
+
+    const checkInputsValid = ():void => {
+      countTrue = 0;
+
+      Array.from(inputs).forEach((input) => {
+        if (!(input instanceof HTMLInputElement)) {
+          return;
+        }
+
+        if(input.classList.contains('input-valid')) {
+          countTrue++
+        }
+
+        this.checkChangeInput(input)
+      })
+
+      console.log(countTrue)
+    }
+
+    elementBtn.addEventListener('click', checkInputsValid)
+  }
+
+  public showErrors(arr: string[]): string {
+    const createError = arr[0];
+    return createError;
+  }
+
 }
