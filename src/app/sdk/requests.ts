80e9d18b95ec4e85df180c24c37f43f5acad9b1e
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-08-25 17:41:37 +0300

    Revert "Merge branch 'release/login-registration-main' of https://github.com/KateGoncharik/eCommerce-Application into release/login-registration-main"
    This reverts commit 8ef8f53803d626f54ee2d6ddda7efc88e03b17be, reversing
    changes made to fc7a80396765baca1624980cd242232525c4094f.

--- src/app/sdk/requests.ts
+++ src/app/sdk/requests.ts
@@ -10,46 +10,38 @@ export const createUser = async (form: DataUser): Promise<number | undefined> =>
   try {
     const request = await getApiRoot().customers().post(form).execute();
     return request.statusCode;
-  } catch (e) {
-    console.log('Error: no connection to server');
+  } catch (err) {
+    console.log(err);
   }
 };
 
-export async function isUserExist(email: string): Promise<boolean | void> {
-  try {
-    const result = await getApiRoot()
-      .customers()
-      .get({ queryArgs: { where: `email="${email}"` } })
-      .execute();
-    return result.body.count > 0;
-  } catch (err) {
-    console.log('Error: no connection to server');
-  }
+export async function isUserExist(email: string): Promise<boolean> {
+  const result = await getApiRoot()
+    .customers()
+    .get({ queryArgs: { where: `email="${email}"` } })
+    .execute();
+  return result.body.count > 0;
 }
 
 export async function authorizeUser(email: string, password: string): Promise<void | string> {
-  try {
-    return await withPasswordFlowClient(email, password)
-      .login()
-      .post({ body: { email: email, password: password } })
-      .execute()
-      .then(
-        (result) => {
-          rememberAuthorizedUser(result.body.customer);
-        },
-        (errorResponse: ClientResponse<ErrorResponse>) => {
-          const emailInput = safeQuerySelector<HTMLInputElement>('.email-input', document);
-          markInputAsInvalid(emailInput);
-          const passwordInput = safeQuerySelector<HTMLInputElement>('.password-input', document);
-          markInputAsInvalid(passwordInput);
+  return await withPasswordFlowClient(email, password)
+    .login()
+    .post({ body: { email: email, password: password } })
+    .execute()
+    .then(
+      (result) => {
+        rememberAuthorizedUser(result.body.customer);
+      },
+      (errorResponse: ClientResponse<ErrorResponse>) => {
+        const emailInput = safeQuerySelector<HTMLInputElement>('.email-input', document);
+        markInputAsInvalid(emailInput);
+        const passwordInput = safeQuerySelector<HTMLInputElement>('.password-input', document);
+        markInputAsInvalid(passwordInput);
 
-          if (errorResponse.body.message === 'Customer account with the given credentials not found.') {
-            return 'Wrong email or password. Try again or register';
-          }
-          return errorResponse.body.message;
+        if (errorResponse.body.message === 'Customer account with the given credentials not found.') {
+          return 'Wrong email or password. Try again or register';
         }
-      );
-  } catch (err) {
-    console.log('Error: no connection to server');
-  }
-}
+        return errorResponse.body.message;
+      }
+    );
+} //TODO make catch instead of then
