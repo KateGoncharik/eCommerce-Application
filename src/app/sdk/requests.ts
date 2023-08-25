c7bbc44ae60f69bb014ad6c01d476f3677ed83ff
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2023-08-25 18:33:30 +0300

    fix: errors from server

--- src/app/sdk/requests.ts
+++ src/app/sdk/requests.ts
@@ -10,38 +10,46 @@ export const createUser = async (form: DataUser): Promise<number | undefined> =>
   try {
     const request = await getApiRoot().customers().post(form).execute();
     return request.statusCode;
-  } catch (err) {
-    console.log(err);
+  } catch (e) {
+    console.log('Error: no connection to server');
   }
 };
 
-export async function isUserExist(email: string): Promise<boolean> {
-  const result = await getApiRoot()
-    .customers()
-    .get({ queryArgs: { where: `email="${email}"` } })
-    .execute();
-  return result.body.count > 0;
+export async function isUserExist(email: string): Promise<boolean | void> {
+  try {
+    const result = await getApiRoot()
+      .customers()
+      .get({ queryArgs: { where: `email="${email}"` } })
+      .execute();
+    return result.body.count > 0;
+  } catch (err) {
+    console.log('Error: no connection to server');
+  }
 }
 
 export async function authorizeUser(email: string, password: string): Promise<void | string> {
-  return await withPasswordFlowClient(email, password)
-    .login()
-    .post({ body: { email: email, password: password } })
-    .execute()
-    .then(
-      (result) => {
-        rememberAuthorizedUser(result.body.customer);
-      },
-      (errorResponse: ClientResponse<ErrorResponse>) => {
-        const emailInput = safeQuerySelector<HTMLInputElement>('.email-input', document);
-        markInputAsInvalid(emailInput);
-        const passwordInput = safeQuerySelector<HTMLInputElement>('.password-input', document);
-        markInputAsInvalid(passwordInput);
+  try {
+    return await withPasswordFlowClient(email, password)
+      .login()
+      .post({ body: { email: email, password: password } })
+      .execute()
+      .then(
+        (result) => {
+          rememberAuthorizedUser(result.body.customer);
+        },
+        (errorResponse: ClientResponse<ErrorResponse>) => {
+          const emailInput = safeQuerySelector<HTMLInputElement>('.email-input', document);
+          markInputAsInvalid(emailInput);
+          const passwordInput = safeQuerySelector<HTMLInputElement>('.password-input', document);
+          markInputAsInvalid(passwordInput);
 
-        if (errorResponse.body.message === 'Customer account with the given credentials not found.') {
-          return 'Wrong email or password. Try again or register';
+          if (errorResponse.body.message === 'Customer account with the given credentials not found.') {
+            return 'Wrong email or password. Try again or register';
+          }
+          return errorResponse.body.message;
         }
-        return errorResponse.body.message;
-      }
-    );
-} //TODO make catch instead of then
+      );
+  } catch (err) {
+    console.log('Error: no connection to server');
+  }
+}
