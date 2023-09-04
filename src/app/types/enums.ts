1feeae8e0b109dfafe757bf376a15533e3e6c3e3
Irina <girishkaizh@gmail.com>
2023-09-05 00:33:48 +0400

    feat: add breadcrumbs and filters to catalog page (EA-101, EA-106)

--- src/app/types/enums.ts
+++ src/app/types/enums.ts
@@ -4,3 +4,13 @@ export enum Country {
   Spain = 'Spain',
   Australia = 'Australia',
 }
+
+export enum Colors {
+  Red = '#ff0000',
+  Green = '#00ff00',
+  Blue = '#0000ff',
+  Pink = '#ff00ff',
+  Orange = '#ffa500',
+  Gold = '#ffd700',
+  Grey = '#808080',
+}
