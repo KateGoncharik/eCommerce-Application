05a62fbb47888657640c6667a30c3c4c2485c00a
laleks6 <lomanovsaha98@gmail.com>
2023-09-17 23:03:46 +0300

    refactor: change style card about us

--- src/app/pages/about-us.ts
+++ src/app/pages/about-us.ts
@@ -1,6 +1,7 @@
 import { Page } from '@templates/page';
 import { el } from 'redom';
-import avatar from '@icons/avatar_test.jpg';
+import avatarA from '@icons/avatar_a.jpg';
+import avatarI from '@icons/avatar_i.jpg';
 // import gitIcon from '@icons/git_icon.png'
 export class AboutUs extends Page {
   constructor() {
@@ -13,37 +14,52 @@ export class AboutUs extends Page {
 
   private createBlockAboutUs(): HTMLElement {
     const blockAboutUs = el('.block-about-us', [
-      el('.card', [
-        el('img.card-img', { src: avatar }),
-        el('block-info', [
-          el('h4.card-name', 'Lomanov Aleksandr'),
-          el('p.card-roles', 'Student developer'),
-          el(
-            'p.description-card',
-            'fjakfjawfjwa ifjawkfja lfkjafk lsja ksfljaklaafjajwfji3j 39qjfafja fajfiajfaw faijfaf fla,f a f,alfaf fla;f alfkalf aflakfa fkalf; kaf;l ka;f ka;f akf ;afk al;f '
-          ),
-          el('.card-links', [
-            el('a.card-cv-link', 'More about me'),
-            el('a.card-git-link', 'Github', { href: 'https://github.com/', target: '_blank' }),
+      el('about-us-description', 'afafafa'),
+      el('.about-us', [
+        el('.card', [
+          el('.block-card-img', [el('img.card-img', { src: avatarA })]),
+          el('.block-info', [
+            el('h4.info-name', 'ALEKSANDR'),
+            el('p.card-roles', 'STUDENT DEVELOPER'),
+            el(
+              'p.description-card',
+              'fjakfjawfjwa ifjawkfja lfkjafk lsja ksfljaklaafjajwfji3j 39qjfafja fajfiajfaw faijfaf fla,f a f,alfaf fla;f alfkalf aflakfa fkalf; kaf;l ka;f ka;f akf ;afk al;f '
+            ),
+            el('.card-links', [
+              el('a.card-cv-link', 'More about me'),
+              el('a.card-git-link', 'Github', { href: 'https://github.com/', target: '_blank' }),
+            ]),
           ]),
         ]),
-      ]),
-      el('.card', [
-        el('img.card-img', { src: avatar }),
-        el('block-info', [
-          el('h4.card-name'),
-          el('span.card-roles'),
-          el('p.card-description'),
-          el('.card-links', [el('a.card-git-link', [el('img.icon-git')]), el('a.card-cv-link')]),
+        el('.card', [
+          el('.block-card-img', [el('img.card-img', { src: avatarI })]),
+          el('.block-info', [
+            el('h4.info-name', 'IRINA'),
+            el('p.card-roles', 'STUDENT DEVELOPER'),
+            el(
+              'p.description-card',
+              'fjakfjawfjwa ifjawkfja lfkjafk lsja ksfljaklaafjajwfji3j 39qjfafja fajfiajfaw faijfaf fla,f a f,alfaf fla;f alfkalf aflakfa fkalf; kaf;l ka;f ka;f akf ;afk al;f '
+            ),
+            el('.card-links', [
+              el('a.card-cv-link', 'More about me'),
+              el('a.card-git-link', 'Github', { href: 'https://github.com/', target: '_blank' }),
+            ]),
+          ]),
         ]),
-      ]),
-      el('.card', [
-        el('img.card-img', { src: avatar }),
-        el('block-info', [
-          el('h4.card-name'),
-          el('span.card-roles'),
-          el('p.card-description'),
-          el('.card-links', [el('a.card-git-link', [el('img.icon-git')]), el('a.card-cv-link')]),
+        el('.card', [
+          el('.block-card-img', [el('img.card-img', { src: avatarA })]),
+          el('.block-info', [
+            el('h4.info-name', 'KATE'),
+            el('p.card-roles', 'STUDENT DEVELOPER'),
+            el(
+              'p.description-card',
+              'fjakfjawfjwa ifjawkfja lfkjafk lsja ksfljaklaafjajwfji3j 39qjfafja fajfiajfaw faijfaf fla,f a f,alfaf fla;f alfkalf aflakfa fkalf; kaf;l ka;f ka;f akf ;afk al;f '
+            ),
+            el('.card-links', [
+              el('a.card-cv-link', 'More about me'),
+              el('a.card-git-link', 'Github', { href: 'https://github.com/', target: '_blank' }),
+            ]),
+          ]),
         ]),
       ]),
     ]);
